6204cdbb58bce6731125a924bfc975c555a17e9a
Dan Green <danngreen1@gmail.com>
2019-08-23 16:44:29 -0700

    output_offset function made (not connected to any UI)

--- inc/envelope_out.h
+++ inc/envelope_out.h
@@ -7,4 +7,5 @@
 #include <stm32f0xx.h>
 
 void output_envelope(uint32_t dacval);
+void output_offset(void);
 void test_envout(void);
--- src/envelope_out.c
+++ src/envelope_out.c
@@ -50,6 +50,22 @@ void output_envelope(uint32_t dacval)
 	}
 	//Todo: use red if 5V is locked
 	update_pwm(env5V, PWM_5VENVLED_B);
+void output_offset(void)
+{
+	int16_t offset = 1024 - (analog[POT_OFFSET].lpf_val>>2);
+
+	update_pwm(offset, PWM_ENV);
+	update_pwm(0, PWM_5VENV);
+
+	if (offset<512) {
+		set_led_brightness((512-offset)*2, PWM_ENVLED_B);
+		set_led_brightness(0, PWM_ENVLED_R);
+	} else {
+		set_led_brightness(0, PWM_ENVLED_B);
+		set_led_brightness((offset-512)*2, PWM_ENVLED_R);
+	}
+
+	set_led_brightness(0, PWM_5VENVLED_B);
 }
 
 void test_envout(void)
