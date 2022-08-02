f390fe278bf1fd857643d714f951d683b2f28552
Dan Green <danngreen1@gmail.com>
2022-08-02 15:14:50 -0700

    f423 receives app to same sector as it runs
    Theres only one sector (128kB), so it actually receives into RAM and doesn:t touch flash until it's done receiving. Thus there's no point in erasing and writing to a receiving sector, and then erasing and copyiing to the app sector.

--- src/f423-drivers/flash_layout.hh
+++ src/f423-drivers/flash_layout.hh
@@ -4,26 +4,23 @@ constexpr uint32_t NumFlashSectors = 8;
 
 constexpr uint32_t get_sector_addr(uint32_t sector_num) {
 	constexpr uint32_t SECTORS[] = {
-		0x08000000,
-		0x08004000,
-		0x08008000,
-		0x0800C000,
-		0x08010000,
-		0x08020000,
-		0x08040000,
-		0x08060000,
+		0x08000000, //Sector 0: 16kB
+		0x08004000, //Sector 1: 16kB
+		0x08008000, //Sector 2: 16kB
+		0x0800C000, //Sector 3: 16kB
+		0x08010000, //Sector 4: 64kB
+		0x08020000, //Sector 5: 128kB
+		0x08040000, //Sector 6: 128kB
+		0x08060000, //Sector 7: 128kB
 		0x08080000, //end
 	};
 	return (sector_num <= NumFlashSectors) ? SECTORS[sector_num] : 0;
 }
 
-constexpr inline uint32_t BootloaderFlashAddr = get_sector_addr(0);
-constexpr inline uint32_t SettingsFlashAddr = get_sector_addr(2);
-constexpr inline uint32_t AppFlashAddr = get_sector_addr(3);
-constexpr inline uint32_t BootloaderReceiveAddr = get_sector_addr(6);
+constexpr inline uint32_t BootloaderFlashAddr = get_sector_addr(0);	  //32kB for bootloader
+constexpr inline uint32_t SettingsFlashAddr = get_sector_addr(2);	  //16kB for settings
+constexpr inline uint32_t AppFlashAddr = get_sector_addr(5);		  //128kB for app
+constexpr inline uint32_t BootloaderReceiveAddr = get_sector_addr(5); //Receive into same address, no point in copying
 
-static_assert(SettingsFlashAddr - BootloaderFlashAddr == 32 * 1024);				   //bootloader
-static_assert(AppFlashAddr - SettingsFlashAddr == 16 * 1024);						   //Settings
-static_assert(BootloaderReceiveAddr - AppFlashAddr == 208 * 1024);					   //App
-static_assert(get_sector_addr(NumFlashSectors) - BootloaderReceiveAddr == 256 * 1024); //receiving firmware
-static_assert(get_sector_addr(NumFlashSectors + 1) == 0);
+//Note: sector size is 128kB, but we can make the firmware update wav file smaller by forcing the app max to be 64kB
+constexpr inline uint32_t BootloaderReceiveSectorSize = 64 * 1024;
