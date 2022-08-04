60d380807dd9ed555410bc806d531a5a3341aad5
Dan Green <danngreen1@gmail.com>
2022-08-04 14:20:38 -0700

    debouncer uses mdrivlib pin

--- src/dig_inouts.cc
+++ src/dig_inouts.cc
@@ -10,36 +10,4 @@ void init_dig_inouts(void) {
 	DigIO::TrigJack init_trigjack{mdrivlib::PinPull::Down};
 	DigIO::EOJack init_eof{mdrivlib::PinPull::None, mdrivlib::PinSpeed::Medium, mdrivlib::PinOType::PushPull};
 	DigIO::ClockBusOut init_clockbus{mdrivlib::PinPull::None, mdrivlib::PinSpeed::Medium, mdrivlib::PinOType::PushPull};
-
-	//GPIO_InitTypeDef gpio = {0};
-
-	//ALL_GPIO_RCC_ENABLE();
-
-	////Configure outputs
-	//gpio.Mode = GPIO_MODE_OUTPUT_PP;
-	//gpio.Speed = GPIO_SPEED_FREQ_HIGH;
-	//gpio.Pull = GPIO_NOPULL;
-	//gpio.Pin = TRIGOUT_Pin;
-	//HAL_GPIO_Init(TRIGOUT_GPIO_Port, &gpio);
-	//// gpio.Pin = DEBUG_Pin;
-	//// HAL_GPIO_Init(DEBUG_GPIO_Port, &gpio);
-
-	////Configure inputs with pullup
-	//gpio.Mode = GPIO_MODE_INPUT;
-	//gpio.Pull = GPIO_PULLUP;
-
-	//gpio.Pin = PING_BUT_Pin;
-	//HAL_GPIO_Init(PING_BUT_GPIO_Port, &gpio);
-	//gpio.Pin = CYCLE_BUT_Pin;
-	//HAL_GPIO_Init(CYCLE_BUT_GPIO_Port, &gpio);
-
-	////Configure inputs with pulldown
-	//gpio.Pull = GPIO_PULLDOWN;
-
-	//gpio.Pin = TRIG_JACK_Pin;
-	//HAL_GPIO_Init(TRIG_JACK_GPIO_Port, &gpio);
-	//gpio.Pin = PING_JACK_Pin;
-	//HAL_GPIO_Init(PING_JACK_GPIO_Port, &gpio);
-	//gpio.Pin = AUXTRIG_JACK_Pin;
-	//HAL_GPIO_Init(AUXTRIG_JACK_GPIO_Port, &gpio);
 }
--- src/shareddrv/debounced_digins.cc
+++ src/shareddrv/debounced_digins.cc
@@ -10,6 +10,10 @@ static mdrivlib::Timekeeper digintmr;
 
 static void debounce_irq(void);
 
+extern volatile uint32_t pingtmr;
+extern volatile uint32_t ping_irq_timestamp;
+extern volatile uint8_t using_tap_clock;
+
 void init_debouncer() {
 	HAL_StatusTypeDef err;
 
@@ -29,12 +33,17 @@ void init_debouncer() {
 static void debounce_irq(void) {
 	uint32_t pin_read;
 
-	debouncers[PING_BUTTON].register_state(PINGBUT ? 1 : 0);
-	debouncers[CYCLE_BUTTON].register_state(CYCLEBUT ? 1 : 0);
-	debouncers[TRIGGER_JACK].register_state(TRIG_JACK_READ ? 1 : 0);
-	debouncers[CYCLE_JACK].register_state(AUXTRIG_JACK_READ ? 1 : 0);
-	debouncers[PING_JACK].register_state(PING_JACK_READ ? 1 : 0);
+	debouncers[PING_BUTTON].register_state(DigIO::PingBut::read() ? 1 : 0);
+	debouncers[CYCLE_BUTTON].register_state(DigIO::CycleBut::read() ? 1 : 0);
+	debouncers[TRIGGER_JACK].register_state(DigIO::TrigJack::read() ? 1 : 0);
+	debouncers[CYCLE_JACK].register_state(DigIO::CycleJack::read() ? 1 : 0);
+	debouncers[PING_JACK].register_state(DigIO::PingJack::read() ? 1 : 0);
 
+	if (debouncers[PING_JACK].is_just_pressed()) {
+		ping_irq_timestamp = pingtmr;
+		pingtmr = 0;
+		using_tap_clock = 0;
+	}
 	for (auto [dig, deb] : zip(digin, debouncers)) {
 		dig.state = deb.is_high() ? 1 : 0;
 		if (deb.is_just_pressed())
